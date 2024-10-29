a12420dc6371cc3d7ad6e5815b70c23e169f54dc
khrte <tichiio.side@gmail.com>
2024-10-28 21:03:35 -0700

    yes

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,32 +1,16 @@
-// Kaz Harte 
-// October 26th, 2024
+//Kaz Harte
+// October 28th, 2024
 
 
-// Variables
-var MyMainRide = {
-  make: "Mercedes",
-  model: "Convertible",
-  color: "black",
-  year: 2000,
-  age: function() { 
-    return 2024 - this.year;
-      }
+function OtherName() {
+  var NameUser = window.prompt("Dude, I'm trying things out! ");
+  console.log("NameUser =", NameUser);
+
+  var newName = NameUser.toLowerCase().split("").sort().join("");
+  console.log("NewName =", newName);
+
+  return newName
 }
 
-var MyTransport = [ 
-  'bus', 'car', 'bike', 'walk'
-  ] 
-
-
-
-
-  // output
-  document.writeln(
-    "Yeah, I get around in my " + MyTransport[1] + ". Its a " + MyMainRide.color + " " + MyMainRide.make + "<br>"
-  );
-
-  document.writeln("My Main Ride: <pre>",
-    JSON.stringify(MyMainRide, null, '\t'), "/<pre>"
-  );
-
-  console.log("Yeah I get around in my " + MyTransport[1] + ". Its a " + MyMainRide.color + " " + MyMainRide.make)
+//output
+document.writeln("LIAR! You're name is actually: ", OtherName(), "<br>");
