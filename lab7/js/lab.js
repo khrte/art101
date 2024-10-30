afd99955bda542e103a050f195fa4e6089a7169c
khrte <tichiio.side@gmail.com>
2024-10-29 19:42:07 -0700

    Lab 8

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,11 +1,27 @@
 //Kaz Harte
 // October 28th, 2024
 
+function NameFile() {
+  var UserName = window.prompt("Dude, whats your FREAKIN name? ");
+  console.log("UserName =", UserName );
+  // makes things sortable for array
+  var nameArray = UserName.split('');
+  console.log("nameArray =", nameArray);
+  // organizes everything
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  // bringing everything back together
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+
+  return nameSorted
+
+}
 
 function OtherName() {
   var NameUser = window.prompt("Dude, I'm trying things out! ");
   console.log("NameUser =", NameUser);
-
+  // sorts name in one line
   var newName = NameUser.toLowerCase().split("").sort().join("");
   console.log("NewName =", newName);
 
--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,32 +1,27 @@
 // Kaz Harte 
-// October 26th, 2024
+// October 29th, 2024
 
 
 // Variables
-var MyMainRide = {
-  make: "Mercedes",
-  model: "Convertible",
-  color: "black",
-  year: 2000,
-  age: function() { 
-    return 2024 - this.year;
-      }
-}
 
-var MyTransport = [ 
-  'bus', 'car', 'bike', 'walk'
-  ] 
+function TheFunctionEver(x) {return (x + 2);}
+
+// testing function!
+console.log("2 + 2 = ", TheFunctionEver(2))
+console.log("63 + 2 = ", TheFunctionEver(63))
+
+// defined array
+array = [55, 42, 100, 78]
+  console.log("This is what my array looks like: ", array);
 
 
+// Adding to whole Array
+var MapResults = array.map(TheFunctionEver);
+
+  console.log("The array if they were two values higher: ", MapResults);
 
 
-  // output
-  document.writeln(
-    "Yeah, I get around in my " + MyTransport[1] + ". Its a " + MyMainRide.color + " " + MyMainRide.make + "<br>"
-  );
+// subtracting actually. 
+var MapResults = array.map(function(x){return x - 6;})
 
-  document.writeln("My Main Ride: <pre>",
-    JSON.stringify(MyMainRide, null, '\t'), "/<pre>"
-  );
-
-  console.log("Yeah I get around in my " + MyTransport[1] + ". Its a " + MyMainRide.color + " " + MyMainRide.make)
+  console.log("Subtraction results: ", MapResults);
