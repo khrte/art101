b447139fd9427582719cc88da29dac399dbf4ba7
khrte <tichiio.side@gmail.com>
2024-11-07 19:57:30 -0800

    Lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,32 +1,27 @@
+
 // Kaz Harte 
 // October 26th, 2024
 
 
-// Variables
-var MyMainRide = {
-  make: "Mercedes",
-  model: "Convertible",
-  color: "black",
-  year: 2000,
-  age: function() { 
-    return 2024 - this.year;
-      }
+function generateRandomText() {
+  const text = "A young man stands in his bedroom. It just so happens that today, the 13th of April, 2009, is this young man's birthday.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the HS text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random HS-like text
+    return text.slice(randStart, randStart + randLen);
 }
 
-var MyTransport = [ 
-  'bus', 'car', 'bike', 'walk'
-  ] 
+// button makes text squirm around. 
+$("#make-convo").click(function(){
+  // uses function to mix text
+    const newText = generateRandomText();
+  // Prints text
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
 
 
 
 
-  // output
-  document.writeln(
-    "Yeah, I get around in my " + MyTransport[1] + ". Its a " + MyMainRide.color + " " + MyMainRide.make + "<br>"
-  );
-
-  document.writeln("My Main Ride: <pre>",
-    JSON.stringify(MyMainRide, null, '\t'), "/<pre>"
-  );
-
-  console.log("Yeah I get around in my " + MyTransport[1] + ". Its a " + MyMainRide.color + " " + MyMainRide.make)
