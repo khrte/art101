34a599473023d6bb3ca28c3f3b59521740d0e06f
khrte <tichiio.side@gmail.com>
2024-11-25 18:49:01 -0800

    Project.2

--- PROJECT/js/lab.js
+++ PROJECT/js/lab.js
@@ -3,16 +3,24 @@
 colorStats = [
   {hitPoint: "5", STR: "4", WLLPWR: "2"},{hitPoint: "4", STR: "2", WLLPWR: "4"},
   {hitPoint: "2", STR: "1", WLLPWR: "6"}, {hitPoint: "4", STR: "6", WLLPWR: "1"},
-  {hitPoint: "3", STR: "3", WLLPWR: "3"}, {hitPoint: "8", STR: "1", WLLPWR: "1"},
-  {hitPoint: "2", STR: "6", WLLPWR: "6"}
+  {hitPoint: "3", STR: "3", WLLPWR: "2"}, {hitPoint: "8", STR: "1", WLLPWR: "3"},
+  {hitPoint: "2", STR: "6", WLLPWR: "4"}
 
 ]
 //setting selectedColor var so the rest of the code can access it outside of event listener.
 
+//to track wether or not colors are selected.
+let selectedColor = null;
+let selectedColor2 = null;
+let test = null;
+let test2 = null;
+
+//color picker #1
 var colorPicker = document.createElement("input");
   colorPicker.type = "color";
   colorPicker.className = "favcolor";
- 
+
+//adds color picker to sidecontent section
 document.body.appendChild(colorPicker);
 $('.sidecontent').append(colorPicker)
 
@@ -21,17 +29,19 @@ $('.sidecontent').append(colorPicker)
     // Get the selected color value from the event target (the <input> tag)
     var selectedColor = event.target.value;
     console.log("selectedColor", selectedColor);
-    test = ClassSortHash(selectedColor)
-    console.log("Test:", test)
+    test = ClassSortHash(selectedColor);
+    console.log("Test:", test);
+
+
     $("#sidecontent").html(test);
 
-    $('.sidecontent2').html("<div><h1> <span style='color: "+ selectedColor + ";'</span>" + selectedColor + "</h1><p>HitPoints: " + test.hitPoint + 
-      "<br> WillPower: " + test.WLLPWR + 
+    $('.sidecontent2').html("<div><h1> <span style='color: "+ selectedColor + ";'</span>" + selectedColor + "</h1><h2>HitPoints: " + test.hitPoint + 
+      "</h2><p> WillPower: " + test.WLLPWR + 
       "<br> Strength: " + test.STR + "</p></div>");
 });
 
 
-// second instance of color picker
+//color picker #2
 var colorPicker2 = document.createElement("input");
   colorPicker2.type = "color";
   colorPicker2.className = "favcolor";
@@ -43,40 +53,73 @@ $('.mainboxescontent').append(colorPicker2)
     // Get the selected color value from the event target (the <input> tag)
     var selectedColor2 = event.target.value;
     console.log("selectedColor", selectedColor2);
-    test2 = ClassSortHash(selectedColor2)
-    console.log("Test2:", test2)
-    $("#sidecontent").html(test2);
-    console.log("selectedColor2: ", selectedColor2)
+    test2 = ClassSortHash(selectedColor2);
+    console.log("Test2:", test2);
 
-    $('.mainboxescontent2').html("<div><h1><span style='color: " + selectedColor2 + ";'</span>" + selectedColor2 + "</h1><p>HitPoints: " + test2.hitPoint + 
-      "<br> WillPower: " + test2.WLLPWR + 
+    $("#sidecontent").html(test2);
+    
+
+    $('.mainboxescontent2').html("<div><h1><span style='color: " + selectedColor2 + ";'</span>" + selectedColor2 + "</h1><h2>HitPoints: " + test2.hitPoint + 
+      "</h2><p> WillPower: " + test2.WLLPWR + 
       "<br> Strength: " + test2.STR + "</p></div>");
 });
+
  
 //purpose of this was to make this ??? work when color was picked??? 
     //but need: once both colors are picked, make it possible to click fight button. 
 
     //figure this out then take the math assessment....
 
-    $("#button").click(function(){
+$("#button").click(function() {
+  if (test && test2) {
       if (test2.STR > test.STR){
-        test.hitPoint = test.hitPoint - 1;
-      }
-
+        test.hitPoint -= 1;
+      } 
       else if (test2.STR < test.STR){
-        test2.hitPoint = test2.hitPoint - 1;
+        test2.hitPoint -= 1;
       }
-
-      else if (test2.STR == test.STR){
+      else if (test2.STR === test.STR){
           if (test2.WILLPWR < test.WLLPWR){
-            test2.hitPoint = test2.hitPoint - 1;
+            test2.hitPoint -= 1;
           }
           else if (test2.WLLPWR > test.WLLPWR){
-            test.hitPoint = test.hitPoint - 1;
+            test.hitPoint -= 1;
           }
         }
+
+        //create a new box where winner is announced. 
+      if (test2.hitPoint <= 0) {
+        $('.mainboxescontent2').html("<div><h1><span style='color: " + selectedColor2 + ";'</span>" + selectedColor2 + "</h1><h2>HitPoints: " + 0 + 
+          "</h2><p> WillPower: " + test2.WLLPWR + 
+          "<br> Strength: " + test2.STR + "</p></div>");
+        $('#winbox').html("<div><h1>Winner is: " + selectedColor2 + "</h1></div>");
+        //disable button
+        $("#button").prop("disabled", true);
+        //exit when game is over
+        return;
+
+      } else if (test.hitPoint <= 0) {
+        $('.sidecontent2').html("<div><h1><span style='color: " + selectedColor2 + ";'</span>" + selectedColor2 + "</h1><h2>HitPoints: " + 0 + 
+          "</h2><p> WillPower: " + test2.WLLPWR + 
+          "<br> Strength: " + test2.STR + "</p></div>");
+        $('#winbox').html("<div><h1>Winner is: " + selectedColor + "</h1></div>");
+
+        $("#button").prop("disabled", true);
+        return;
+      }
     }
-)
+
+    //update
+    $('.sidecontent2').html("<div><h1><span style='color: " + selectedColor + ";'>" + selectedColor + "</span></h1>" + 
+      "<h2>HitPoints: " + test.hitPoint + "</h2><p> WillPower: " + test.WLLPWR + 
+      "<br> Strength: " + test.STR + "</p></div>");
+
+    $('.mainboxescontent2').html("<div><h1><span style='color: " + selectedColor2 + ";'>" + selectedColor2 + "</span></h1>" + 
+       "<h2>HitPoints: " + test2.hitPoint + "</h2><p> WillPower: " + test2.WLLPWR + 
+      "<br> Strength: " + test2.STR + "</p></div>");
+    }
+  
+   );
 //take string and return hashed checksum of the string
 function checksum(str) {
   var hash = 0; 
@@ -105,7 +148,3 @@ function ClassSortHash(str) {
 
 
 
-//"<h1>" + "<p>You're a <span style='color: " + 
-    //classObj.color + ";'>" + classObj.t + "</span>! </p>" + 
-   // classObj.text + "</h1>" + "<img class=photo src='img/" + 
-    //classObj.image + "'>" 
