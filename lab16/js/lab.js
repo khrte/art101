c216b2d8c232232d469ad58e9a4efb205bc0abea
khrte <tichiio.side@gmail.com>
2024-12-02 15:42:22 -0800

    Lab 16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,32 +1,84 @@
 // Kaz Harte 
-// October 26th, 2024
+// December 2nd, 2024
+const URL = "https://api.nasa.gov/planetary/apod?api_key=RgNBs4TjYNVjwPSQLLvkMtIj4zOflKWVB3TikTry"
 
+//so. obv fake url.
+//modeling this off of other websites I've seen. 
+//lets pretend real url is "https://api.nasa.gov/planetary/photo-1217.json"
+                                                                            //or something. 
+const URLstart = "https://api.nasa.gov/planetary/photo-";
+const URLend = "/json";
+ 
+//button to start function.
+$("#button").click(function(){
+    // call ajax
+    $.ajax(comicObj);
+  })
 
-// Variables
-var MyMainRide = {
-  make: "Mercedes",
-  model: "Convertible",
-  color: "black",
-  year: 2000,
-  age: function() { 
-    return 2024 - this.year;
-      }
+  const comicObj = {
+// The URL for the request (from the api docs)
+  url: URL,
+  type: "GET",
+  dataType: "json",
+  headers: {
+    "Accept": "application/json"
+  },
+  success: ajaxSuccess,
+  error: ajaxError
 }
 
-var MyTransport = [ 
-  'bus', 'car', 'bike', 'walk'
-  ] 
+//this function doesn't do anything and exists for the fake scenario where
+//this api works just like the xkcd one.
+    //pretend like nasa photos can be accessed by number.
+
+function NasaPhotos(num) {
+  if (typeof num === 'undefined') {
+    numStr = "";
+  } else {
+    numStr = num.toString() + "/";
+  }
+  var realURL = URLstart + numStr + URLend;
+  console.log(realURL);
+
+  $.ajax({
+      // The URL for the request (ENDPOINT)
+      url: realURL,
+      // Whether this is a POST or GET request
+      type: "GET",
+  })
+
+}
+    
+  //API success
+    function ajaxSuccess(data) {
+      console.log("Data received:", data);
+      var title = data.title;
+      var img = data.url;
+      //there is no option for alt in this API, here's how I would have done it though. 
+        // var altt = data.alt;
+        // $("#output").html("<h1>" + title + "</h1><img src='" + img + "' alt='" + alt "'>")
+      var photoNum = data.date;
+      console.log(photoNum)
+      $("#output").html("<h1>" + title + "</h1><br><img class='oops' src='" + img + "' alt='No space pic today!'>"
+        + "<div id=futurepast><button id=back>Prev Pic</button>  <button id=forward>Future Pic</button></div>"
+      );
+      
+      // so theres no option to go back or forward on the image with this API, but again heres how I would have done it.
+        //originally considered trying something with the date instead of photo num, however things got too messy when months and not just days were considered. 
+
+      $("#back").click(function(){
+        NasaPhotos(photoNum - 1);
+      });
+      // add event listener to new next button
+      $("#forward").click(function(){
+        NasaPhotos(photoNum + 1);
+      });
+    }
+  
+
+   // create ajax error callback
+   function ajaxError(request,error){
+    console.log("Oh no: ", request, error);
+}
 
 
-
-
-  // output
-  document.writeln(
-    "Yeah, I get around in my " + MyTransport[1] + ". Its a " + MyMainRide.color + " " + MyMainRide.make + "<br>"
-  );
-
-  document.writeln("My Main Ride: <pre>",
-    JSON.stringify(MyMainRide, null, '\t'), "/<pre>"
-  );
-
-  console.log("Yeah I get around in my " + MyTransport[1] + ". Its a " + MyMainRide.color + " " + MyMainRide.make)
